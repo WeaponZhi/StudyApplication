4147f1055ed8194350b098f74a36c1bc20427db0
WeaponZhi <584098488@qq.com>
2017-02-20 23:11:20 +0800

    测试分支
    添加一个Java文件测试版本分支

---
+++ app/src/test/java/com/imooc/studyapplication/TestJava.java
@@ -0,0 +1,19 @@
+package com.imooc.studyapplication;
+
+import org.junit.Test;
+
+/**
+ * TestJava
+ * <p>
+ * author: 张冠之 <br>
+ * time:   2017/02/20 23:09 <br>
+ * e-mail: 584098488@qq.com <br>
+ * </p>
+ */
+
+public class TestJava {
+    @Test
+    public void main(){
+        
+    }
+}
